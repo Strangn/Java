3e490be2db911b2fde50611a402d8333cdb704f8
Your Namegit <strangn10@gmail.com>
2018-09-19 10:11:17 -0400

    Grade converter finished

--- ch02_prj2_gradeconverter/src/GradeConverterApp.java
+++ ch02_prj2_gradeconverter/src/GradeConverterApp.java
@@ -4,29 +4,36 @@ public class GradeConverterApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Letter Grade Converter");
-		
 		Scanner sc = new Scanner(System.in);
-		
+
 		String choice = "y";
-				
+
 		while (choice.equalsIgnoreCase("y")) {
-			System.out.print("Enter degrees in Fahrenhiet: ");
+			System.out.print("Enter numerical grade: ");
 			int numericalGrade = sc.nextInt();
-		
-		String Celcius = "";
-		
-		//convert fahrenhiet to celsius
-		if c = (f-32) * 5/9)  {
-			celsius = "";
-		}
-		
-		System.out.println("Celcius: "+celsius);
-		
-		System.out.println("Continue? (y/n)");
-		choice = sc.next();
+
+			String letterGrade = "";
+
+			// Convert the numericGrade to letterGrade
+			if (numericalGrade >= 88) {
+				letterGrade = "A";
+			} else if (numericalGrade >= 80) {
+				letterGrade = "B";
+			} else if (numericalGrade >= 68) {
+				letterGrade = "C";
+			} else if (numericalGrade >= 60) {
+				letterGrade = "D";
+			} else {
+				letterGrade = "F";
+			}
+
+			System.out.println("Letter grade: " + letterGrade);
+			
+			System.out.println("Continue? (y/n)");
+			choice = sc.next();
 		}
 		sc.close();
-		System.out.println("Goobye, you son of a bitch");
+		System.out.println("Bye");
 	}
 
 }
